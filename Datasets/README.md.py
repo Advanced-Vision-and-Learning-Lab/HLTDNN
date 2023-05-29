ac753f00393b123078a549787a08d8a3dd20259e
Jairn Tasnim Ritu <jariniitju@gmail.com>
2023-05-28 22:17:58 -0500

    Add files via upload

---
+++ Datasets/README.md.py
@@ -0,0 +1,84 @@
+
+"""
+
+# Downloading datasets:
+
+Note: Due to the size of the datasets, the audio files were not 
+upload to the repository. For the HLTDNN paper, only DeepShip dataset was used. 
+Please follow the following instructions
+to ensure the code works. If the dataset is used,
+please cite the appropiate sources (papers, repositories, etc.) as mentioned
+on the webpages and provided here.
+
+##  DeepShip
+
+Please download the [`DeepShip dataset`](https://drive.google.com/drive/folders/1ParUYYFnVme3yNTC_-JTtuhyqjOMZ3rJ?usp=share_link) 
+and follow these instructions:
+
+1. Download and unzip the file
+2. Name the folder `Deepship`
+3. The structure of the `Deepship` folder is as follows:
+'''Deepship/
+    ├── Cargo/
+    │   ├── Cargo1/
+    │   │   ├── audio1.wav
+    │   │   ├── audio2.wav
+    │   │   └── ...
+    │   ├── Cargo2/
+    │   │   ├── audio1.wav
+    │   │   ├── audio2.wav
+    │   │   └── ...
+    │   └── ...
+    ├── Passenger/
+    │   ├── Passenger1/
+    │   │   ├── audio1.wav
+    │   │   ├── audio2.wav
+    │   │   └── ...
+    │   ├── Passenger2/
+    │   │   ├── audio1.wav
+    │   │   ├── audio2.wav
+    │   │   └── ...
+    │   └── ...
+    ├── Tanker/
+    │   ├── Tanker1/
+    │   │   ├── audio1.wav
+    │   │   ├── audio2.wav
+    │   │   └── ...
+    │   ├── Tanker2/
+    │   │   ├── audio1.wav
+    │   │   ├── audio2.wav
+    │   │   └── ...
+    │   └── ...
+    ├── Tug/
+    │   ├── Tug1/
+    │   │   ├── audio1.wav
+    │   │   ├── audio2.wav
+    │   │   └── ...
+    │   ├── Tug2/
+    │   │   ├── audio1.wav
+    │   │   ├── audio2.wav
+    │   │   └── ...
+    │   └── ...
+    └── ...
+
+'''
+
+## <a name="CitingKTH"></a>Citing DeepShip
+
+If you use the DeepShip dataset, please cite the following reference using the following entry.
+
+**Plain Text:**
+
+Tavakoli Targhi, A., Hayman, E., Caputo, B., Mallikar-juna, P., Fritz, M., and  Eklundh, J.O.
+“The kth-tips2 database,” KTH Royal Institute of Technology, 2006.
+
+**BibTex:**
+```
+@article{mallikarjuna2006kth,
+  title={The kth-tips2 database},
+  author={Mallikarjuna, P and Targhi, Alireza Tavakoli and Fritz, Mario and Hayman, Eric and Caputo, Barbara and Eklundh, Jan-Olof},
+  journal={KTH Royal Institute of Technology},
+  year={2006}
+}
+
+```
