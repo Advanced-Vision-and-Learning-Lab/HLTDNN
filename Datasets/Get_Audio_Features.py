317a3d0af05518edfd2848d5ac871ae8c5c3d890
Jairn Tasnim Ritu <jarin.ritu@tamu.edu>
2023-10-30 11:54:28 -0500

    Update Get_Audio_Features.py

--- Datasets/Get_Audio_Features.py
+++ Datasets/Get_Audio_Features.py
@@ -8,10 +8,23 @@ Generate audio feature transforms
 from torchvision import transforms
 import torchaudio.transforms as T
 from nnAudio import features
+import librosa
+import numpy as np
+import pdb
+import torch
+import torch.nn as nn
+from nnAudio import features
 
 
-def Get_Audio_Features(feature, sample_rate=16000, window_length=250, 
-                       hop_length=64, RGB=False, pretrained=False):
+def vqt_lib(x):
+    x_numpy = x.numpy().astype(np.float32)
+    signal = librosa.vqt(x_numpy, sr=16000,hop_length=int((64/1000)*16000),
+                                        n_bins=64)
+    signal = torch.from_numpy(signal)
+    return signal
+
+def Get_Audio_Features(input_features, data, sample_rate=16000, window_length=250, 
+                       hop_length=64, RGB=False, pretrained=False, device="cpu"):
     
     #Convert window and hop length to ms
     window_length /= 1000
@@ -23,131 +36,47 @@ def Get_Audio_Features(feature, sample_rate=16000, window_length=250,
         num_channels = 1
     
     #Based on desired feature, return transformation
-    if feature == 'Mel_Spectrogram':
-        #Return Mel Spectrogram that is 48 x 48
-        
-        signal_transform = T.MelSpectrogram(sample_rate,n_mels=40,win_length=int(window_length*sample_rate),
+    Mel_Spectrogram = nn.Sequential(features.mel.MelSpectrogram(sample_rate,n_mels=40,win_length=int(window_length*sample_rate),
                                             hop_length=int(hop_length*sample_rate),
-                                            n_fft=int(window_length*sample_rate))
+                                            n_fft=int(window_length*sample_rate), verbose=False), nn.ZeroPad2d((1,4,0,4)))
         
-        train_transforms = transforms.Compose([
-            signal_transform,
-            transforms.Pad((1,4,0,4)),
-            transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-        ])
-        
-        test_transforms = transforms.Compose([
-                signal_transform,
-                transforms.Pad((1,4,0,4)),
-                transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-            ])
-        
-       
-    elif feature == 'MFCC':
+    
         #Return MFCC that is 16 x 48
-        signal_transform = T.MFCC(sample_rate=sample_rate, n_mfcc=16, 
-                                        melkwargs={"n_fft": int(window_length*sample_rate), 
-                                                   "win_length": int(window_length*sample_rate), 
-                                                   "hop_length": int(hop_length*sample_rate),
-                                                   "n_mels": 48, "center": False})
-        
-        train_transforms = transforms.Compose([
-            signal_transform,
-            transforms.Pad((1,0,4,0)),
-            transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-    
-        ])
-        test_transforms = transforms.Compose([
-                signal_transform,
-                transforms.Pad((1,0,4,0)),
-                transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-            ])
-        
+    MFCC = nn.Sequential(features.mel.MFCC(sr=sample_rate, n_mfcc=16, 
+                                        n_fft=int(window_length*sample_rate), 
+                                                win_length=int(window_length*sample_rate), 
+                                                hop_length=int(hop_length*sample_rate),
+                                                n_mels=48, center=False, verbose=False), nn.ZeroPad2d((1,0,0,0)))
 
-    elif feature == 'STFT': 
-    #Return STFT that is 48 x 48
-        signal_transform = features.STFT(sr=sample_rate,n_fft=int(window_length*sample_rate), 
-                                         hop_length=int(hop_length*sample_rate),
-                                         win_length=int(window_length*sample_rate), 
-                                         output_format='Magnitude',
-                                         freq_bins=48,verbose=False)
-            
-        train_transforms = transforms.Compose([
-            signal_transform,
-            transforms.Pad((1,0,0,0)),
-            transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-        ])
-
-        
-        test_transforms = transforms.Compose([
-                signal_transform,
-                transforms.Pad((1,0,0,0)),
-                transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-            ])
-        
-    elif feature == 'GFCC':
-        #Return GFCC that is 64 x 48
-        signal_transform = features.Gammatonegram(sr=sample_rate,
-                                                  hop_length=int(hop_length*sample_rate),
-                                                  n_fft=int(window_length*sample_rate),
-                                                  verbose=False,n_bins=64)
-        
-        train_transforms = transforms.Compose([
-            signal_transform,
-            transforms.Pad((1,0,0,0)),
-            transforms.Lambda(lambda x: x.repeat(num_channels,1,1))
-        ])
-
-        test_transforms = transforms.Compose([
-                signal_transform,
-                transforms.Pad((1,0,0,0)),
-                transforms.Lambda(lambda x: x.repeat(num_channels,1,1))
-            ])
-
-    elif feature == 'CQT':
-        #Return CQT that is 64 x 48
-        signal_transform = features.CQT(sr=sample_rate, n_bins=64, 
+        #Return STFT that is 48 x 48
+    STFT = nn.Sequential(features.STFT(sr=sample_rate,n_fft=int(window_length*sample_rate), 
                                         hop_length=int(hop_length*sample_rate),
-                                        verbose=False)
+                                        win_length=int(window_length*sample_rate), 
+                                        output_format='Magnitude',
+                                        freq_bins=48,verbose=False), nn.ZeroPad2d((1,0,0,0)))
         
-        train_transforms = transforms.Compose([
-            signal_transform,
-            transforms.Pad((1,0,0,0)),
-            transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-        ])
+        #Return GFCC that is 64 x 48
+    GFCC = nn.Sequential(features.Gammatonegram(sr=sample_rate,
+                                                hop_length=int(hop_length*sample_rate),
+                                                n_fft=int(window_length*sample_rate),
+                                                verbose=False,n_bins=64), nn.ZeroPad2d((1,0,0,0)))
+        
 
-        test_transforms = transforms.Compose([
-                signal_transform,
-                transforms.Pad((1,0,0,0)),
-                transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-            ])
+        #Return CQT that is 64 x 48
+    CQT = nn.Sequential(features.CQT(sr=sample_rate, n_bins=64, 
+                                        hop_length=int(hop_length*sample_rate),
+                                        verbose=False), nn.ZeroPad2d((1,0,0,0)))
         
-    elif feature == 'VQT':
         #Return VQT that is 64 x 48
-        signal_transform = features.VQT(sr=sample_rate,hop_length=int(hop_length*sample_rate),
-                                        n_bins=64,earlydownsample=False,verbose=False)
+    VQT = nn.Sequential(features.VQT(sr=sample_rate,hop_length=int(hop_length*sample_rate),
+                                        n_bins=64,earlydownsample=False,verbose=False), nn.ZeroPad2d((1,0,0,0)))
 
-        train_transforms = transforms.Compose([
-            signal_transform,
-            transforms.Pad((1,0,0,0)),
-            transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-        ])
-
-    
-        test_transforms = transforms.Compose([
-                signal_transform,
-                transforms.Pad((1,0,0,0)),
-                transforms.Lambda(lambda x: x.repeat(num_channels,1,1)),
-            ])
-
-    else:
-        raise RuntimeError('{} not implemented'.format(feature))
+    feature_extract = {'Mel_Spectrogram': Mel_Spectrogram, 'MFCC': MFCC, 'STFT': STFT, 'GFCC': GFCC, 'CQT': CQT, 'VQT': VQT}
             
+    transformed_features = []
     
-    #If pretrained, add ImageNet normalization
-    if (pretrained and RGB):
-        norm_transform = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        train_transforms = transforms.Compose(train_transforms,norm_transform)
-        test_transforms = transforms.Compose(test_transforms,norm_transform)
+    for feature in input_features:
+        transformed_features.append(feature_extract[feature](data))
+    combined_features = torch.stack(transformed_features, dim=1)
     
-    return { 'train':train_transforms, 'test': test_transforms}
+    return combined_features
